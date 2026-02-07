4c9b7169f8ebe3aeafec94d5baf66abad4e4bb50
Waiker <mamai.94@mail.ru>
2026-02-08 01:50:21 +0500

    обложка для профиля

--- assets/js/profile-config.js
+++ assets/js/profile-config.js
@@ -2,11 +2,11 @@
 const PROFILE_CONFIG = {
   /* beltColors: массив объектов с id, label, icon. Поддерживается старый формат (массив строк). */
   beltColors: [
-    { id: 'belyy', label: 'белый', icon: 'assets/img/belts/white.png', color: '#e8e6e3', colorLight: '#f5f4f2' },
-    { id: 'siniy', label: 'синий', icon: 'assets/img/belts/blue.png', color: '#2d5a87', colorLight: '#3d6a9a' },
-    { id: 'purpurnyy', label: 'пурпурный', icon: 'assets/img/belts/purple.png', color: '#5a3d6a', colorLight: '#6b4c7a' },
-    { id: 'korichnevyy', label: 'коричневый', icon: 'assets/img/belts/brown.png', color: '#4a3728', colorLight: '#5c4535' },
-    { id: 'chernyy', label: 'черный', icon: 'assets/img/belts/black.png', color: '#1a1a1a', colorLight: '#2a2a2a' }
+    { id: 'belyy', label: 'белый', icon: 'assets/img/belts/white.png', color: '#d4d0c8', colorLight: '#f0ede8' },
+    { id: 'siniy', label: 'синий', icon: 'assets/img/belts/blue.png', color: '#1e4a6e', colorLight: '#3a7ab0' },
+    { id: 'purpurnyy', label: 'пурпурный', icon: 'assets/img/belts/purple.png', color: '#4a2d5a', colorLight: '#7a4a8e' },
+    { id: 'korichnevyy', label: 'коричневый', icon: 'assets/img/belts/brown.png', color: '#3d2a1e', colorLight: '#6b4a35' },
+    { id: 'chernyy', label: 'черный', icon: 'assets/img/belts/black.png', color: '#0d0d0d', colorLight: '#2d2d2d' }
   ],
   divisions: [
     { id: 'gi', label: 'Gi' },
